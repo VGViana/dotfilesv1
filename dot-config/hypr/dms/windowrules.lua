ffbe50927e3cf010a2876591d2da0da7ae111d81
VGViana <akumanomips4@gmail.com>
2026-09-05 10:42:44 -0300

    sync(fedora): update dotfiles 2026-09-05 10:42:44

--- dot-config/hypr/dms/windowrules.lua
+++ dot-config/hypr/dms/windowrules.lua
@@ -3,3 +3,6 @@
 
 -- DMS-RULE: id=dms-floating-windows, name=DMS Floating Windows
 hl.window_rule({ match = { class = "^com.danklinux.dms$" }, float = true })
+
+-- DMS-RULE: id=wr_1788611780288767252, name=btop
+hl.window_rule({ match = { class = "^btop$" }, float = true })
